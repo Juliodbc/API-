3ac6f6ee9d8dbc5c1e76049df90679f6eb0defc7
Your Name <you@example.com>
2025-04-22 16:49:28 -0300

    API_3

--- index.php
+++ index.php
@@ -4,21 +4,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Exception\HttpNotFoundException;
-use Projetux\Service\TarefaException;
-use Projetux\Infra\Debug;
-use Projetux\Math\Basic;
+use Projetux\Service\TarefasService;
+use Projetux\math\Basic;
  
 require __DIR__ . '/vendor/autoload.php';
  
-$app = AppFactory::create();
- 
-// middleware é um evento que ocorre antes da requisição chegar na rota.
- 
+$app = AppFactory::create(); 
+
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
     Request $request,
     Throwable $exception,
-    bool $displayErrorDetails,
+    bool $displayerrorDetails,
     bool $logErrors,
     bool $logErrorDetails
 ) use ($app) {
@@ -28,58 +25,95 @@ $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
         ->withStatus(404);
 });
  
-$app->get('/math/quadrado/{num1}', function (Request $request, Response $response, array $args) {
-$basic = new Basic();
-$resultado = $basic->quadrado($args["num1"]);
-$response->getBody()->write((string)$resultado);
-return $response;   
+$app->get('/teste/soma/{num1}/{num2}', function (Request $request, Response $response, array $args) {
+    $basic = new basic();
+    $resultado = $basic->soma($args['num1'], $args['num2']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
 });
+ 
+$app->get('/teste/subtrai/{num1}/{num2}', function (Request $request, Response $response, array $args) {
+    $basic = new basic();
+    $resultado = $basic->subtrai($args['num1'], $args['num2']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
+});
+ 
+ 
+$app->get('/teste/multi/{num1}/{num2}', function (Request $request, Response $response, array $args) {
+    $basic = new basic();
+    $resultado = $basic->multi($args['num1'], $args['num2']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
+});
+ 
+ 
+$app->get('/teste/div/{num1}/{num2}', function (Request $request, Response $response, array $args) {
+    $basic = new basic();
+    $resultado = $basic->div($args['num1'], $args['num2']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
+});
+ 
+ 
+$app->get('/teste/pot/{num1}', function (Request $request, Response $response, array $args) {
+    $basic = new basic();
+    $resultado = $basic->pot($args['num1']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
+});
+ 
+ 
+$app->get('/teste/raiz/{num1}', function (Request $request, Response $response, array $args) {
+    $basic = new Basic();
+    $resultado = $basic->raiz($args['num1']);
+    $response->getBody()->write((string) $resultado);
+    return $response;
+});
+ 
+$app->get('/tarefas', function (Request $request, Response $response, array $args) {
+    $tarefas_service = new TarefasService();
+    $tarefas = $tarefas_service->getAllTarefas();
+    $response->getBody()->write(json_encode($tarefas));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+ 
 $app->post('/tarefas', function (Request $request, Response $response, array $args) {
-    $paramentos = (array) $request->getParsedBody();
-    if (!array_key_exists('titulo', $paramentos) || empty($paramentos['titulo'])) {
-        $response->getBody()->write(json_encode([
-            "mensagem" => "titulo é obrigatorio"
-        ]));
-        return $response->withHeader('content-type', 'application/json')->withStatus(400);
-    }
-    $tarefa = array_merge(['titulo' => '', 'concluido' => false], $paramentos);
-    $tarefa_service = new TarefasService();
-    $tarefa_service->createTarefa($tarefa);
+   $parametros = (array) $request->getParsedBody();
+   if(!array_key_exists('titulo', $parametros) || empty($parametros['titulo'])){
+    $response->getBody()->write(json_encode([
+      "mensagem" => "titulo é obrigatorio"
+    ]));
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+   }
+   $tarefas = array_merge(['titulo' => '', 'concluido' => false], $parametros);
+   $tarefas_service = new TarefasService();
+   $tarefas_service->createTarefa($tarefas);
  
-    return $response->withStatus(201);
-});
- 
- 
-$app->delete('/tarefas', function (Request $request, Response $response, array $args) {
- 
-    return $response->withStatus(204);
-});
-$app->put('/tarefas', function (Request $request, Response $response, array $args) {
-    return $response->withStatus(201);
+   return $response->withStatus(201);
 });
  
 $app->delete('/tarefas/{id}', function (Request $request, Response $response, array $args) {
     $id = $args['id'];
- 
+    $tarefas_service = new TarefasService();
+    $tarefas_service->deleteTarefas($id);
     return $response->withStatus(204);
 });
  
 $app->put('/tarefas/{id}', function (Request $request, Response $response, array $args) {
     $id = $args['id'];
-    $dados_para_atualizar = (array) $request->getParsedBody();
+    $dados_para_atualizar = json_decode($request->getBody()->getContents(), true);
     if (array_key_exists('titulo', $dados_para_atualizar) && empty($dados_para_atualizar['titulo'])) {
         $response->getBody()->write(json_encode([
             "mensagem" => "titulo é obrigatorio"
         ]));
         return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
     }
-    $tarefa_service = new TarefasService();
-    $tarefa_service->updateTarefa($id, $dados_para_atualizar);
+    $tarefas_service = new TarefasService();
+    $tarefas_service->updateTarefa($id,$dados_para_atualizar);
  
     return $response->withStatus(201);
 });
  
  
- 
 $app->run();
- 
